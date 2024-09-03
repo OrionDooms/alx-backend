73e8b637df48e9a02bfa0bd49a41b518187d2684
Orion <Orion45.dd@gmail.com>
2024-09-03 11:51:30 -0700

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
+"""FIFO caching"""
 from collections import OrderedDict
-
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
+    """class FIFOCache that inherits from BaseCaching and is a caching system"""
     def __init__(self):
+        """Initialize with object attributes, super() inherit attributes
+        from parent class and the OrderedDict class and assign it to the
+        self.cache_data attribute"""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if key is not None and item is not None:
+        """Assign to the dictionary self.cache_data the items value
+        for the key"""
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    first_key = next(iter(self.cache_data))
+                    print("DISCARD: {}".format(first_key))
+                    self.cache_data.pop(first_key)
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
-                print(f"DISCARD: {first_key}")
-
 
     def get(self, key):
-        if key is None or key in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key is not None or key in self.cache_data:
+            return self.cache_data[key]
+        return None
