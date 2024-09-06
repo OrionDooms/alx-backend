4fb5c4fed80d9bacc381a633086ccfca41632b64
Orion <Orion45.dd@gmail.com>
2024-09-06 08:00:03 -0700

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,16 +16,17 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Assign to the dictionary self.cache_data the items value
         for the key"""
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            if BaseCaching.MAX_ITEMS < len(self.cache_data):
+                first_key, _ = self.cache_data.popitem(False)
                 print("DISCARD: {}".format(first_key))
-                self.cache_data.pop(first_key)
-        self.cache_data[key] = item
+            else:
+                return
+        else:
+            return
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """if key is None or key not in self.cache_data:
+            return None"""
+        return self.cache_data(key)
