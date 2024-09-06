ac9198c457e67e2784dd865e85bf2b721ef40008
Orion <Orion45.dd@gmail.com>
2024-09-06 12:44:26 -0700

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -29,6 +29,6 @@ class LIFOCache(BaseCaching):
             return
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
+        """if key is None or key not in self.cache_data:
+            return None"""
         return self.cache_data[key]
