a80577d5c9c0e05b49e980f7f1717c429c66bf3a
OrionDooms <orion45.dd@gmail.com>
2024-09-13 16:13:56 +0200

    setup a basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+"""Setting up a basic flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """A index.html that outputs Welcome to Holberton as page title
+    and Hello world as a header."""
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
